4f8da4f37577fce6fd1f94d0f69805f406676303
ankit.gautam <ankit.gautam@flipkart.com>
2022-02-28 05:50:52 +0530

    fix: fix

--- src/store/videos/slice.js
+++ src/store/videos/slice.js
@@ -1,33 +1,30 @@
 // Completely unneccessary no need of redux
-import createAsyncThunkPlus from "create-async-thunk-plus";
 import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import createAsyncThunkPlus from "../../thunkPlus";
+
 const entityAdapter = createEntityAdapter({
 	selectId: (entity) => entity.id,
 });
 
-export const playVideo = createAsyncThunkPlus(
-	"videos/create",
-	({ socket, ...options }) => socket.emitPromise("playVideo", options)
+export const playVideo = createAsyncThunkPlus("videos/create", ({ socket, ...options }) =>
+	socket.emitPromise("playVideo", options)
 );
 
-export const fullscreenVideo = createAsyncThunkPlus(
-	"videos/fullscreen",
-	({ socket, ...options }) => socket.emitPromise("fullscreenVideo", options)
+export const fullscreenVideo = createAsyncThunkPlus("videos/fullscreen", ({ socket, ...options }) =>
+	socket.emitPromise("fullscreenVideo", options)
 );
-
 
 const slice = createSlice({
 	name: "videos",
 	initialState: entityAdapter.getInitialState(),
-	reducers: {
-	},
+	reducers: {},
 	extraReducers: {
 		...playVideo.reducers,
-		...fullscreenVideo.reducers
+		...fullscreenVideo.reducers,
 	},
 });
 export const { setActive } = slice.actions;
 export const selectors = entityAdapter.getSelectors((state) => {
-	return state.tabs
+	return state.tabs;
 });
 export default slice.reducer;
